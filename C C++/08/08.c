24fb7e826053a143952cbe69ed96221921f45e36
karasu_corp <aquillae777@gmail.com>
2024-04-11 21:53:33 -0300

    up

--- C C++/08/08.c
+++ C C++/08/08.c
@@ -3,11 +3,11 @@
 int main() {
     int i;
     int array[] = {1, 2, 3, 4, 5, 6};
-    int tamanho_do_array = sizeof(array) / sizeof(array[0]); // Calcula o tamanho do array
+    int tamanho_do_array = sizeof(array) / sizeof(array[0]); 
     
     for (i = 0; i < tamanho_do_array; i++) {
         printf("%d ", array[i]);
     }
-
+    
     return 0;
 }
